de995161f117374d787a6ed053699229a75e25b9
Christopher Ketchum <cketchum@ucsc.edu>
2016-04-14 20:42:28 -0700

    Annotated methods to finish in project one

--- codebase/rbf/rbfm.cc
+++ codebase/rbf/rbfm.cc
@@ -3,9 +3,8 @@
 RecordBasedFileManager* RecordBasedFileManager::_rbf_manager = 0;
 
 RecordBasedFileManager* RecordBasedFileManager::instance()
-{
-    if(!_rbf_manager)
-        _rbf_manager = new RecordBasedFileManager();
+{// to be implemented in project 1
+    ordBasedFileManager();
 
     return _rbf_manager;
 }
@@ -35,13 +34,16 @@ RC RecordBasedFileManager::closeFile(FileHandle &fileHandle) {
 }
 
 RC RecordBasedFileManager::insertRecord(FileHandle &fileHandle, const vector<Attribute> &recordDescriptor, const void *data, RID &rid) {
+    // to be implemented in project 1
     return -1;
 }
 
 RC RecordBasedFileManager::readRecord(FileHandle &fileHandle, const vector<Attribute> &recordDescriptor, const RID &rid, void *data) {
+    // to be implemented in project 1
     return -1;
 }
 
 RC RecordBasedFileManager::printRecord(const vector<Attribute> &recordDescriptor, const void *data) {
+    // to be implemented in project 1
     return -1;
 }
