f29ce3602c055beb3791d4a3028bdad14ce1f46b
Gage Michael Sperry <gsperry@unix2.lt.ucsc.edu>
2016-04-21 08:57:11 -0700

    printRecord properly checking null

--- codebase/rbf/rbfm.cc
+++ codebase/rbf/rbfm.cc
@@ -78,7 +78,7 @@ RC RecordBasedFileManager::readRecord(FileHandle &fileHandle, const vector<Attri
 RC RecordBasedFileManager::printRecord(const vector<Attribute> &recordDescriptor, const void *data) {
 	RC err = 0;			//to store most recent error
 	
-	int numAttr = recordDescriptor.size();	//get the number of attributes to find the number of bytes needed to describe the null attributes
+/*	int numAttr = recordDescriptor.size();	//get the number of attributes to find the number of bytes needed to describe the null attributes
 	int numNullBytes = ceil((float)(numAttr)/8);		//get the number of null bytes
 	vector<char> nullBytes;	//stores the bytes representing null attributes
 	vector<bool> nullAttr;	//stores the bits representing null attributes after they've been split apart from bytes.
@@ -91,7 +91,7 @@ RC RecordBasedFileManager::printRecord(const vector<Attribute> &recordDescriptor
 	for(int i = 0; i < numNullBytes; i++)
 	{
 		char* aByte = (char *) malloc(sizeof(char*)+1);
-		int curBit = 0xA0; //for tracking the current bit when anding it with the nullBytes		
+		int curBit = 0x80; //for tracking the current bit when anding it with the nullBytes		
 		aByte = (char *) data; 
 
 		void* aByte2 = malloc(numNullBytes);
@@ -100,7 +100,7 @@ RC RecordBasedFileManager::printRecord(const vector<Attribute> &recordDescriptor
 
 		for(int j = 0; j<8; j++)
 		{
-			int bitwise = ((*((char*)aByte)) & curBit);
+			int bitwise = ((*((char*)aByte2)) & curBit);
 			if (curBit == bitwise)	//if the bit we're looking at is 1, then the value of attribute j is null
 			{
 				nullAttr[j] = 1;
@@ -109,11 +109,45 @@ RC RecordBasedFileManager::printRecord(const vector<Attribute> &recordDescriptor
 			curBit = curBit>>1;	//double current bit, making it look like the next bit.
 		}
 	}
+*/
+	//round 2, fight
+	//
+	//
+	int numAttr = recordDescriptor.size();		//get the number of attributes to find the number of bytes needed to describe the null attributes of the record
+	int numNullBytes = ceil((float)(numAttr)/8.0);	//get the number of null bytes
+	char* nullBytes;	//store the bytes representing null attributes;
+	vector<bool> nullAttr;	//store the bits representing null attributes after being split apart.
+
+	//allocate some space for bytes and bits
+	nullBytes = (char*) malloc(numNullBytes);
+	nullAttr.resize(numNullBytes*8);
+
+	memcpy((void*)nullBytes, data, numNullBytes); //copy the number of null bytes from data into our nullBytes char array, filling nullBytes
+
+	for(int i = 0; i < numNullBytes; i++)
+	{	
+		//curBit starts at 128, 10000000 in binary, for ANDing with the bytes later
+		//decreases by a factor of 2 each time, 01000000, 00100000 and so on.
+		int curBit = 0x80;
+
+		for(int j = 0; j < 8; j++)	
+		{
+			if(curBit == (curBit & *(nullBytes + i*sizeof(char))))
+			{
+				nullAttr[j] = 1;
+			}
+			else nullAttr[j] = 0;
+			
+			cout<<curBit<<" "<<nullAttr[j]<<" "<<(curBit & *(nullBytes /*+ i*sizeof(char)*/))<<endl;
+			curBit = curBit>>1;
+			
+		}
+	}	
 
 
 	
 	//print rest.
-	unsigned auxOffset = numNullBytes;
+	unsigned auxOffset = numNullBytes*sizeof(char);
 	for (int i = 0; i < numAttr; i++)
 	{
 		string attrName = recordDescriptor[i].name;
@@ -128,26 +162,15 @@ RC RecordBasedFileManager::printRecord(const vector<Attribute> &recordDescriptor
 		else switch(recordDescriptor[i].type)
 		{
 			case 0 : 	int dataInt;
-					dataInt = *(int*)((char*)data + offset);
 					cout<<dataInt<<" ";
 					break;
 			case 1 :	float dataFloat;
-					dataFloat = *(float*)((char*)data + offset);
 					cout<<dataFloat<<" ";
 					break;
-/* not working*/	case 2 :	string dataVarChar;	/**fix me**/
-					char* tempChar = (char*)malloc(length * sizeof(char)); 
-				
-					strncpy(tempChar, (char*)((char*)data + offset), length);
-				
-					dataVarChar.assign((char*)((char*)data), length);
-				
-					memcpy((void*)tempChar,(void*)((char*)data+offset), length*sizeof(char)); 
-				
-					printf("%s ", tempChar);
+			case 2 :		
 					break;
 		}
-		auxOffset = length + auxOffset;
+		auxOffset = length*sizeof(char) + auxOffset;
 	} 
 	cout<<endl;
     return 0;
